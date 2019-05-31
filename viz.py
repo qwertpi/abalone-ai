9c8d35ce7cf8813b803005dcbd9cf1fabc49efb9
qwertpi <qwertpi@users.noreply.github.com>
2019-05-31 10:09:35 +0100

    Linted code

--- viz.py
+++ viz.py
@@ -1,12 +1,14 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-data=np.loadtxt("data.csv", delimiter=",")
-for i in range(0,8):
+data = np.loadtxt("data.csv", delimiter=",")
+
+for i in range(0, 8):
     X = data[:,i]
     print(X)
     plt.hist(X)
     plt.show()
+    
 Y = data[:,8]
 print(Y)
 plt.hist(Y)
